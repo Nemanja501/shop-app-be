bba344b98633b7fd5bf0505a18f84f004e37fadf
Nemanja501 <nemanjavulin5@gmail.com>
2025-02-26 18:13:21 +0100

    added login and getting user by id

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -17,7 +19,37 @@ class UserController extends Controller
         ]);
 
         return response()->json([
-            'message' => 'User registered succesfully'
+            'message' => 'User registered successfully'
+        ]);
+    }
+
+    public function login(LoginRequest $request) {
+        $validated = $request->validated();
+        $user = User::where('email', $validated['email'])->first();
+        if(!$user || Hash::check($validated['password'], $user->password)) {
+            return response()->json([
+                'error' => 'Invalid credentials'
+            ], 422);
+        }
+
+        $token = $user->createToken($user->username . $user->email)->plainTextToken;
+        return response()->json([
+            'message' => 'Login successful',
+            'userId' => $user->id,
+            'token' => $token
+        ]);
+    }
+
+    public function getById(string $id) {
+        $user = User::where('id', $id)->first();
+        if(!$user) {
+            return response()->json([
+                'error' => 'User not found'
+            ], 404);
+        }
+        return response()->json([
+            'message' => 'Fetched user successfully',
+            'user' => $user
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
 Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::get('/user/{id}', [UserController::class, 'getById'])->middleware('auth:sanctum');
